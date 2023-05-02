ffe2cc758a56955964a4fdbe5aa9da3048158920
Adophilus <uchenna19of@gmail.com>
2023-05-02 05:45:31 +0100

    updated layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,9 +2,9 @@ import type { PropsWithChildren } from 'react'
 
 const PageLayout = (props: PropsWithChildren<unknown>) => {
   return (
-    <main className="flex flex-col min-h-screen">
-      <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
-        <div className="flex border-b border-slate-400">
+    <main className="flex justify-center min-h-screen">
+      <div className="self-stretch w-full border-x border-slate-400 md:max-w-2xl">
+        <div className="h-full border-b border-slate-400">
           {props.children}
         </div>
       </div>
